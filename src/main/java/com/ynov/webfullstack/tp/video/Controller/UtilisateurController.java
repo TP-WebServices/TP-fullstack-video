47fde0c282acb064a4947f37d759f19fdc0afdb3
Tom MOSTI <tommosti1@gmail.com>
2024-03-28 16:13:35 +0100

    add exception

--- src/main/java/com/ynov/webfullstack/tp/video/Controller/UtilisateurController.java
+++ src/main/java/com/ynov/webfullstack/tp/video/Controller/UtilisateurController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -52,8 +53,13 @@ public class UtilisateurController {
     }
 
     @GetMapping("/role/{title}")
-    public Iterable<Utilisateur> getUtilisateursByRoleTitle(@PathVariable String title) {
-        return utilisateurRepository.findUtilisateursByRoleTitle(title);
+    public ResponseEntity getUtilisateursByRoleTitle(@PathVariable String title) {
+        List<Utilisateur> utilisateurs = utilisateurRepository.findUtilisateursByRoleTitle(title);
+        if(utilisateurs.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No Utilisateur found with this role");
+        }else{
+            return ResponseEntity.ok(utilisateurs);
+        }
     }
 
     @GetMapping("/similar/{username}")
